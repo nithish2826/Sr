4582e1ff47b1946ce675d9df63dc4dbffdc4ce38
nithish2826 <snithishkumar2826@gmail.com>
2025-11-13 21:08:12 +0530

    s

--- Trello-Project/src/App.jsx
+++ Trello-Project/src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Scrap from "./components/Scrap";
+import { Analytics } from "@vercel/analytics/next"
 
 const App = () => {
   return (
     <>
+    <Analytics />
     <Scrap></Scrap>
     </>
   );
